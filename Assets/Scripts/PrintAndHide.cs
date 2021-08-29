8845494416ad10df29ea271605e505e9f2d5960e
Patrick Korczak <36663050+pat5080@users.noreply.github.com>
2021-08-29 11:20:40 +1000

    100 percent progress

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     private int i;
+    public GameObject blue;
+    public GameObject red;
+
+    private int number;
 
     public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        
+        blue = GameObject.FindGameObjectWithTag("Blue");
+        red = GameObject.FindGameObjectWithTag("Red");
+        number = Random.Range(200, 251);
+        
+        rend = blue.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -18,5 +28,16 @@ public class PrintAndHide : MonoBehaviour
     {
         ++i;
         Debug.Log(this.gameObject.name + ":" + i);
+
+        if (i == 100)
+        {
+            red.SetActive(false);
+        }
+
+        if (i == number)
+        {
+            rend.enabled = false;
+        }
+        
     }
 }
